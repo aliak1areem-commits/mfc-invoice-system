3d36832fed0e18ae5a327a64474955bfdb3d6c7b
aliak1areem-commits <ali.a.k1areem@gmail.com>
2026-09-02 10:56:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -219,10 +219,10 @@ function processDump(headers, rows){
     const qtyOpen = key.qtyOpen ? parseNum(r[key.qtyOpen]) : 0;
     const qty = key.qty ? parseNum(r[key.qty]) : qtyOpen;
 
-    if(selfBilling || deletedBlocked){
-      excluded++;
-      return;
-    }
+    if(selfBilling || deletedBlocked || qtyOpen<=0){
+  excluded++;
+  return;
+}
 
     available.push({
       poRaw: po,
